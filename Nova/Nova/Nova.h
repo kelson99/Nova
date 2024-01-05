f438d9367915eafa2b9609e3b7268275bdf1aa35
Kelson Hartle <kelson99hartle@icloud.com>
2024-01-05 11:23:19 -0700

    Added starting xcode project

---
+++ Nova/Nova/Nova.h
@@ -0,0 +1,18 @@
+//
+//  Nova.h
+//  Nova
+//
+//  Created by Kelson Hartle on 1/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Nova.
+FOUNDATION_EXPORT double NovaVersionNumber;
+
+//! Project version string for Nova.
+FOUNDATION_EXPORT const unsigned char NovaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nova/PublicHeader.h>
+
+
