0f3739750482e824fa61f7fdea9c827424d3a87c
Kelson Hartle <kelson99hartle@icloud.com>
2024-01-06 15:08:33 -0700

    Added supabase info

---
+++ Nova/Nova/Supabase/SupabaseAPIInfo.swift
@@ -0,0 +1,13 @@
+//
+//  SupabaseAPIInfo.swift
+//  Nova
+//
+//  Created by Kelson Hartle on 1/6/24.
+//
+
+import Foundation
+
+public struct SupabaseAPIInfo {
+    static let url = "https://hiiozvdrpyhetuahwken.supabase.co"
+    static let key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpaW96dmRycHloZXR1YWh3a2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0OTgxNTEsImV4cCI6MjAyMDA3NDE1MX0.VQFp_BPAVTkblZvIZX-GIhbvmjCfusKIJXCCMqCwTuc"
+}
