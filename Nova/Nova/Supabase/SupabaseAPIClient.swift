01132c1e160339547a2c3c140a792d1a07aca7e4
Kelson Hartle <kelson99hartle@icloud.com>
2024-01-07 13:57:36 -0700

    test_read_deliversDecodableType_onSuccesful200Response

--- Nova/Nova/Supabase/SupabaseAPIClient.swift
+++ Nova/Nova/Supabase/SupabaseAPIClient.swift
@@ -14,9 +14,32 @@ final public class SupabaseAPIClient: SupabaseClient {
     }
 
     public func readFromDatabase<T:Decodable>(tableName: SupabaseTableName) async throws -> T {
-        let data = try await databaseClient.read(from: .dailyChallenges)
+        let data = try await databaseClient.read(from: tableName)
         let decoder = JSONDecoder()
         let decodedData = try decoder.decode(T.self, from: data)
         return decodedData
+    }
+}
+
+public struct DailyChallenge: Decodable, Equatable {
+    public let id: Int
+    public let hint: String
+    public let correctAnswer: String
+    public let challengeText: String
+    public let challengeID: Int
+    
+    public init(id: Int, hint: String, correctAnswer: String, challengeText: String, challengeID: Int) {
+        self.id = id
+        self.hint = hint
+        self.correctAnswer = correctAnswer
+        self.challengeText = challengeText
+        self.challengeID = challengeID
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, hint
+        case correctAnswer = "correct_answer"
+        case challengeText = "challenge_text"
+        case challengeID = "challenge_id"
     }
 }
--- Nova/NovaTests/SupabaseAPIClientTests.swift
+++ Nova/NovaTests/SupabaseAPIClientTests.swift
@@ -10,7 +10,6 @@ import Nova
 import XCTest
 
 final class SupabaseAPIClientTests: XCTestCase {
-    struct Testing: Decodable {}
     func test_supabaseAPIClient_doesNotContact_DBClient_uponInit() {
         let (_, dbClient) = makeSUT()
         XCTAssertTrue(dbClient.messages.isEmpty)
@@ -20,7 +19,7 @@ final class SupabaseAPIClientTests: XCTestCase {
         let (sut, dbClient) = makeSUT()
         dbClient.throwErrorDuringRetrieval(anyNSError())
         do {
-            let _: Testing = try await sut.readFromDatabase(tableName: .dailyChallenges)
+            let _: DailyChallenge = try await sut.readFromDatabase(tableName: .dailyChallenges)
         } catch {}
         XCTAssertEqual(dbClient.messages, [.read(.dailyChallenges)])
     }
@@ -28,12 +27,36 @@ final class SupabaseAPIClientTests: XCTestCase {
     func test_read_deliversErrorOnDBError() async throws {
         let (sut, dbClient) = makeSUT()
         let expectedError = anyNSError()
-        dbClient.throwErrorDuringRetrieval(anyNSError())
+        dbClient.throwErrorDuringRetrieval(expectedError)
         do {
-            let _: Testing = try await sut.readFromDatabase(tableName: .dailyChallenges)
+            let _: DailyChallenge = try await sut.readFromDatabase(tableName: .dailyChallenges)
         } catch {
             XCTAssertEqual(expectedError, error as NSError)
         }
+    }
+    
+    func test_read_deliversDecodableType_onSuccesful200Response() async throws {
+        let (sut, dbClient) = makeSUT()
+        let expectedChallenge = DailyChallenge(id: 1, hint: "", correctAnswer: "", challengeText: "", challengeID: 0)
+        let expectedData = makeDailyChallengeData(expectedChallenge)
+        dbClient.returnResultDuringRetrieval(data: expectedData)
+        do {
+            let dailyChallenges: [DailyChallenge] = try await sut.readFromDatabase(tableName: .dailyChallenges)
+            XCTAssertEqual(dailyChallenges, [expectedChallenge])
+        } catch {
+            XCTFail("Expected no errors - \(error)")
+        }
+    }
+    
+    private func makeDailyChallengeData(_ dailyChallenge: DailyChallenge) -> Data {
+        let json = [[
+            "id": dailyChallenge.id,
+            "hint": dailyChallenge.hint,
+            "correct_answer": dailyChallenge.correctAnswer,
+            "challenge_text": dailyChallenge.challengeText,
+            "challenge_id": dailyChallenge.challengeID
+        ]] as [[String : Any]]
+        return try! JSONSerialization.data(withJSONObject: json)
     }
     
     private func makeSUT() -> (sut: SupabaseAPIClient, dbClient: MockSupabaseDatabaseClient) {
@@ -59,6 +82,10 @@ final class SupabaseAPIClientTests: XCTestCase {
                 throw error
             }
             return returnResult!
+        }
+        
+        func returnResultDuringRetrieval(data: Data) {
+            returnResult = data
         }
         
         func throwErrorDuringRetrieval(_ error: Error) {
