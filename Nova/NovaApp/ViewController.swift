f21ae384a1a40e820968950cd6c259728eec13b2
Kelson Hartle <kelson99hartle@icloud.com>
2024-01-07 19:16:40 -0700

    Added Nova iOS App

---
+++ Nova/NovaApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NovaApp
+//
+//  Created by Kelson Hartle on 1/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.backgroundColor = .green
+    }
+
+
+}
+
