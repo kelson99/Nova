e74d9649d898d091a6d2735e3ab9e3e3b711aa71
Kelson Hartle <kelson99hartle@icloud.com>
2024-01-08 11:44:56 -0700

    test_resultIsReceivedFromSupabaseTable

--- Nova/DailyChallengeEndToEndTests/DailyChallengeEndToEndTests.swift
+++ Nova/DailyChallengeEndToEndTests/DailyChallengeEndToEndTests.swift
@@ -5,9 +5,22 @@
 //  Created by Kelson Hartle on 1/8/24.
 //
 
+import Nova
 import XCTest
 
 final class DailyChallengeEndToEndTests: XCTestCase {
-
-
+    func test_resultIsReceivedFromSupabaseTable() async throws {
+        let dailyChallenges = try await getDailyChallenges()
+        XCTAssertTrue(!dailyChallenges.isEmpty)
+    }
+    
+    private func getDailyChallenges() async throws -> [DailyChallenge] {
+        let supabaseDatabaseContact = SupabaseDatabaseContact()
+        let supabaseAPIClient = SupabaseAPIClient(databaseClient: supabaseDatabaseContact)
+        let dailyChallengeLoader = RemoteDailyChallengeLoader(supabaseAPIClient: supabaseAPIClient)
+        trackForMemoryLeaks(supabaseDatabaseContact)
+        trackForMemoryLeaks(supabaseAPIClient)
+        trackForMemoryLeaks(dailyChallengeLoader)
+        return try await dailyChallengeLoader.load()
+    }
 }
--- Nova/Nova/Supabase/SupabaseDatabaseContact.swift
+++ Nova/Nova/Supabase/SupabaseDatabaseContact.swift
@@ -10,6 +10,8 @@ import Supabase
 public final class SupabaseDatabaseContact: SupabaseDatabaseClient {
     private let client = SupabaseClient(supabaseURL: SupabaseAPIInfo.url, supabaseKey: SupabaseAPIInfo.key)
     
+    public init() {}
+    
     public func read(from tableName: SupabaseTableName) async throws -> Data {
         do {
             let supabaseResponse = try await client.database
