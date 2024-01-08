e089437305e2c7881ee6a5c76e41d6b224e3d4e7
Kelson Hartle <kelson99hartle@icloud.com>
2024-01-08 10:58:00 -0700

    DailyChallengeEndToEndTests target added

---
+++ Nova/DailyChallengeEndToEndTests/DailyChallengeEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  DailyChallengeEndToEndTests.swift
+//  DailyChallengeEndToEndTests
+//
+//  Created by Kelson Hartle on 1/8/24.
+//
+
+import XCTest
+
+final class DailyChallengeEndToEndTests: XCTestCase {
+
+
+}
